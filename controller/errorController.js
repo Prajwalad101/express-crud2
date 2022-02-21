01453c7ef2d8e353270c2b7ca982616a9ea7dc5e
Prajwal Adhikari <prajwalad101@gmail.com>
2022-02-21 20:05:55 +0545

    set database errors as operational

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,3 +1,21 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDb = (err) => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const message = 'Invalid input data';
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -36,7 +54,15 @@ const globalErrorHandler = (err, req, res, next) => {
   if (nodeEnvironment === 'development') {
     sendErrorDev(err, res);
   } else if (nodeEnvironment === 'production') {
-    sendErrorProd(err, res);
+    let error;
+
+    // under these conditions, mark error as operational
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFieldsDb(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+
+    // send the new error as response
+    sendErrorProd(error, res);
   }
 };
 
