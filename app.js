3b10c1efec3344aaef371e9cb06d7bcf24574cb9
Prajwal Adhikari <prajwalad101@gmail.com>
2022-02-21 17:30:13 +0545

    handle unhandled routes

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ app.use(express.json()); // Parses the req body
 app.use('/api/v1/cars', carRouter);
 
 module.exports = app;
+
+// TODO: IMPLEMENT ERROR HANDLING
+
+app.all('*', (req, res, next) => {
+  // send an error response back to the client
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
+// TODO: 2. Implement a global error middleware function
+// TODO: 3. Refactor try/catch blocks by catching errors in a seperate async function
+// TODO: 4. Add 404 (Not found) errors
+// TODO: 5. Seperate development vs production errors
+// TODO: 6. Handle errors in database (validation, fields, id's)
+// TODO: 7. Handle unhandled rejections
+// TODO: 8. Handle uncaught exceptions
